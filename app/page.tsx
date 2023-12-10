deab56850848d703baf4770d931b53f6c09e7f3f
Scott Anderson <skaclmbr@gmail.com>
2023-12-10 16:12:06 -0500

    test edit

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Get confused by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
       </div>
