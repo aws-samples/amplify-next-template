15c43ed31da3204f63cda7c6c22b99c63a0761f1
devopssoso <113772268+devopssoso@users.noreply.github.com>
2023-12-20 16:07:12 +0100

    webpage + stylé

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,70 @@
-// Importez les modules React nécessaires
 import React from 'react';
+import styled from 'styled-components';
+
+// Styles avec Styled Components
+const Container = styled.div`
+  font-family: 'Arial', sans-serif;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+const Header = styled.header`
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+`;
+
+const Main = styled.main`
+  margin-top: 20px;
+`;
+
+const Section = styled.section`
+  margin-bottom: 20px;
+`;
+
+const Footer = styled.footer`
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
 
 // Composant de la page principale
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <header>
+    <Container>
+      <Header>
         <h1>Mon Site</h1>
-        {/* Ajoutez d'autres éléments du header si nécessaire */}
-      </header>
+        <p>Une description accrocheuse de votre site.</p>
+      </Header>
 
-      <main>
-        <section>
+      <Main>
+        <Section>
           <h2>Bienvenue sur Mon Site</h2>
           <p>C'est un endroit génial où vous pouvez trouver toutes sortes d'informations intéressantes.</p>
-        </section>
+        </Section>
+
+        <Section>
+          <h2>Nos Services</h2>
+          <ul>
+            <li>Service 1</li>
+            <li>Service 2</li>
+            <li>Service 3</li>
+          </ul>
+        </Section>
 
         {/* Ajoutez d'autres sections et contenus de votre choix */}
-      </main>
+      </Main>
 
-      <footer>
+      <Footer>
         <p>&copy; 2023 Mon Site. Tous droits réservés.</p>
-        {/* Ajoutez d'autres éléments de pied de page si nécessaire */}
-      </footer>
-    </div>
+      </Footer>
+    </Container>
   );
 };
 
