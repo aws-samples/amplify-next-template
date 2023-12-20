43cdebfb0786a7d1206d08c6883a0181bbdd8c0b
devopssoso <113772268+devopssoso@users.noreply.github.com>
2023-12-20 15:57:49 +0100

    commit page nodejs

--- app/page.tsx
+++ app/page.tsx
@@ -1,86 +1,30 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// Importez les modules React nécessaires
+import React from 'react';
 
-export default function Home() {
+// Composant de la page principale
+const HomePage: React.FC = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-      </div>
+    <div>
+      <header>
+        <h1>Mon Site</h1>
+        {/* Ajoutez d'autres éléments du header si nécessaire */}
+      </header>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <span>+</span>
-        <Image
-          src="/amplify.svg"
-          alt="Amplify Logo"
-          width={45}
-          height={37}
-          priority
-        />
-      </div>
+      <main>
+        <section>
+          <h2>Bienvenue sur Mon Site</h2>
+          <p>C'est un endroit génial où vous pouvez trouver toutes sortes d'informations intéressantes.</p>
+        </section>
 
-      <div className={styles.grid}>
-        <a
-          href="https://docs.amplify.aws/gen2/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Review documentation for Amplify's code-first DX (Gen 2).</p>
-        </a>
+        {/* Ajoutez d'autres sections et contenus de votre choix */}
+      </main>
 
-        <a
-          href="https://docs.amplify.aws/gen2/start/quickstart/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Quickstart <span>-&gt;</span>
-          </h2>
-          <p>Follow a tutorial to build a fullstack app with Amplify Gen 2.</p>
-        </a>
+      <footer>
+        <p>&copy; 2023 Mon Site. Tous droits réservés.</p>
+        {/* Ajoutez d'autres éléments de pied de page si nécessaire */}
+      </footer>
+    </div>
+  );
+};
 
-        <a
-          href="https://docs.amplify.aws/gen2/build-a-backend/auth/set-up-auth/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Auth <span>-&gt;</span>
-          </h2>
-          <p>Zero-config Auth UI components with social sign-in and MFA.</p>
-        </a>
-
-        <a
-          href="https://docs.amplify.aws/gen2/build-a-backend/data/set-up-data/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Data <span>-&gt;</span>
-          </h2>
-          <p>
-            Fully-typed real-time API with NoSQL database.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default HomePage;
