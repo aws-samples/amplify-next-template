a58d87c6d9ebbeaaf8762f31402577b309db4303
Tobias <tobias@bardino.dk>
2024-01-21 19:32:21 +0100

    First draft landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,22 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>Guldkystens Dartklub</main>;
+  return (
+    <main className={styles.main}>
+      <div className="center">
+        <div className={styles.description}>
+          <h1>Guldkystens Dartklub</h1>
+        </div>
+      </div>
+
+      <Image
+        src="/dart.png"
+        width={800}
+        height={957}
+        alt="Guldkystens Dartklub Logo"
+        style={{ display: "block", marginLeft: "auto", marginRight: "auto" }}
+        priority
+      />
+    </main>
+  );
 }
