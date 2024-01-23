c71bfaafc4a85f67d7abd3cfbfd0533605e80f1f
cdf49 <cdf49@drexel.edu>
2024-01-22 19:27:14 -0500

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Get started editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
       </div>
