20e49ae63bc1e1376474e2a9c9c28f9768b935f1
DawitA <davvit@users.noreply.github.com>
2024-02-04 22:05:47 -0600

    added env var

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <p>development</p>
+      <p>{process.env.ENVIRONMENT}</p>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
