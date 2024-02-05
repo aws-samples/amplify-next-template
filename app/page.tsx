66dcf7e30b8d3766e2fb4c112ddd58106933cf45
DawitA <davvit@users.noreply.github.com>
2024-02-04 21:48:07 -0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <p>development</p>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
