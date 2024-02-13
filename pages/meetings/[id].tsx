9a0ff26c0bb7ff671b1d073cad8707d68151459d
Carsten Koch <carstenbkoch@gmail.com>
2024-02-13 17:34:19 +0100

    fixing page for new meeting

--- pages/meetings/[id].tsx
+++ pages/meetings/[id].tsx
@@ -1,14 +1,13 @@
-import { meetings } from "@/components/demo-data/meetings";
 import Layout from "@/components/layouts/Layout";
 import { useRouter } from "next/router";
 
 export default function MeetingDetailPage() {
   const router = useRouter();
   const id = router.query.id;
-  const meeting = meetings.find((m) => `${m.id}` === id);
+
   return (
-    <Layout title={meeting?.title || "New Meeting"} drawBackBtn>
-      {JSON.stringify(meeting)}
+    <Layout title={"New Meeting"} drawBackBtn>
+      WIP
     </Layout>
   );
 }
