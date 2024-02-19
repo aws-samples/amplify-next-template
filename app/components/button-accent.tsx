396449d4d42a3e7f09201241395d512e01a5cfd2
Darrell Elmore <delmore@2one.tech>
2024-02-18 21:33:03 -0500

    initial color themes set

--- app/components/button-accent.tsx
+++ app/components/button-accent.tsx
@@ -1,16 +1,8 @@
 import Button, { ButtonProps } from "@mui/material/Button";
 import styled from "@mui/material/styles/styled";
 
-const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
-    color: theme.palette.secondary.contrastText,
-    backgroundColor: theme.palette.secondary.dark,
-    '&:hover': {
-        backgroundColor: theme.palette.secondary.light,
-    },
-}));
-
 export default function ButtonAccent(props: any) {
     return (
-        <ColorButton variant="contained">{props.text}</ColorButton>
+        <Button  variant="contained" color="secondary">{props.text}</Button>
     );
 }
--- app/styles/theme.ts
+++ app/styles/theme.ts
@@ -1,6 +1,7 @@
 'use client';
 import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
+import { blue, pink } from '@mui/material/colors'
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -9,9 +10,17 @@ const roboto = Roboto({
 });
 
 const theme = createTheme({
-  typography: {
-    fontFamily: roboto.style.fontFamily,
-  },
+    typography: {
+        fontFamily: roboto.style.fontFamily,
+    },
+    palette: {
+        primary: {
+            main: blue[500],
+        },
+        secondary: {
+            main: pink[500],
+        }
+    },
 });
 
 export default theme;
