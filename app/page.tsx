d38fa326f1edf4d162e8abd9f0597e90d61d8d69
Shahuss1115 <161158995+Shahuss1115@users.noreply.github.com>
2024-02-24 14:54:42 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          This2 needs more detail:Get started by editing&nbsp;
+          This3 needs more detail:Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
       </div>
